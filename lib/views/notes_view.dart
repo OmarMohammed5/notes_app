f3e09911e5cd3adf3bfb8991ef399f186145df96
omarm335 <sdvopjghjervk@gmail.com>
2025-07-26 21:00:20 +0300

    create appBar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -6,32 +6,32 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Colors.black,
-      //   title: Row(
-      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //     children: [
-      //       Text(
-      //         "Notes",
-      //         style: TextStyle(
-      //           fontSize: 30,
-      //           fontWeight: FontWeight.w400,
-      //           color: Colors.white,
-      //         ),
-      //       ),
-      //       Container(
-      //         height: 40,
-      //         width: 40,
-      //         decoration: BoxDecoration(
-      //           shape: BoxShape.rectangle,
-      //           borderRadius: BorderRadius.circular(12),
-      //           color: Color(0xFF4B4B4B),
-      //         ),
-      //         child: Icon(Icons.search, size: 30, color: Colors.white),
-      //       ),
-      //     ],
-      //   ),
-      // ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Notes",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12),
+                color: Color(0xFF4B4B4B),
+              ),
+              child: Icon(Icons.search, size: 30, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
