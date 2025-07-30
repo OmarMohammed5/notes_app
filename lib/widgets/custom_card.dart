755b270b852f557c6b8cf592127bf0b2b3191eee
omarm335 <sdvopjghjervk@gmail.com>
2025-07-30 16:49:57 +0300

    delete note done

--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/add_note_cubit/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 import 'package:intl/intl.dart';
@@ -54,7 +56,10 @@ class CustomCard extends StatelessWidget {
                   ),
                 ),
                 trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    note.delete();
+                    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                  },
                   icon: const Icon(
                     FontAwesomeIcons.trash,
                     color: Colors.black,
